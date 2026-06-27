4b58d206af48370e8dc8118baa3aa09b43ef8ca8
MUKHAMMADDIYOR IBROKHIMOV <imrbek0208@gmail.com>
2026-06-27 23:14:30 +0900

    feat: integration of environmental variables

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+// Architectual pattern(backend suyagi): MVC(model view controller), DI(dependency Injection), MVP(model vieew presenter)
+// MVC = (model view controller)
+// DI = (dependency Injection)
+// MVP = (model vieew presenter)
+
+// Design pattern(backend malum bir bo'lagi): Middleware, Decotar
+// 3 xil turga bo'linar ekan:
+// Creational
+// Structural
+// Behavioral design patterns
+
+import dotenv from 'dotenv';
+dotenv.config();
