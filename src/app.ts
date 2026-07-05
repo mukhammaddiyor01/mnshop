7c6878e7824e8731e5ae28debebf534b5ce5f927
MUKHAMMADDIYOR IBROKHIMOV <imrbek0208@gmail.com>
2026-07-05 16:32:11 +0900

    feat: Integrate middleware sessions

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,15 @@ import router from "./router";
 import routerAdmin from "./router-admin";
 import { MORGAN_FORMAT } from "./libs/config";
 
+import session from "express-session";
+import ConnectMongoDB from "connect-mongodb-session";
+
+const MongoDBStore = ConnectMongoDB(session);
+const store = new MongoDBStore({
+    uri: String(process.env.MONGO_URL),
+    collection:'sessions',
+});
+
 /** 1-Entrance */
 const app = express();
 console.log("__dirname:", __dirname);
@@ -14,6 +23,17 @@ app.use(express.json()); //Rest API
 app.use(morgan(MORGAN_FORMAT));
 
 /** 2-Sessions */
+app.use(
+    session({
+        secret: String(process.env.SESSION_SECRET),
+        cookie: {
+            maxAge: 1000 * 3600 * 3
+        },
+        store: store,
+        resave: true,
+        saveUninitialized: true
+    })
+);
 
 
 /** 3-Views */ 
